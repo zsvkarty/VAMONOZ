d50dd9454cdabbbf3c76dec026f70e4b631f11ca
zsvkarty <georgij.rjabcenko@gmail.com>
2025-03-23 11:27:33 +0100

    Update vite config and build

--- flashcards/vite.config.ts
+++ flashcards/vite.config.ts
@@ -4,13 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/VAMONOZ/',
+  base: '/VAMONOZ/flashcards/',
   build: {
     outDir: 'dist',
     sourcemap: true,
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
       },
     },
   },
